40e5936d4aec10842c4af3bb604cfb36a72dcb4e
JordonMoses <jordon@blackbirdrevolt.com>
2021-01-09 15:19:42 -0600

    mysql Connections & connections.js

---
+++ mysql-connection/connection.js
@@ -0,0 +1,23 @@
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "brooklyn",
+  database: "employee_tracker",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected at " + connection.threadId + "\n");
+});
+
+module.exports = connection;
