6977bba03ab436e5d268d6b844a056d74f8da04d
JordonMoses <jordon@blackbirdrevolt.com>
2021-01-09 14:32:05 -0600

    Schema.sql expanded

--- database/schema.sql
+++ database/schema.sql
@@ -14,10 +14,22 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
-)
+    FOREIGN KEY(department_id)      
+    REFERENCES department(id)      
+    ON DELETE CASCADE 
+);
 
 
 CREATE TABLE employee (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL, 
+    manager_id INT, 
+    FOREIGN KEY(role_id) 
+    REFERENCES role(id) 
+    ON DELETE CASCADE, 
+    FOREIGN KEY(manager_id) 
+    REFERENCES employee(id) 
+    ON DELETE CASCADE
 )
